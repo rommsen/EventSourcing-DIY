4ea1a876570a5f6fb9c44ed0edf9d9dc2b5a7a68
Roman Sachse <roman.sachse@googlemail.com>
2018-08-13 07:35:58 +0200

    bla

--- Types.fs
+++ Types.fs
@@ -1,83 +1,131 @@
-module Types
+module Types =
+
+  // Domain
+  type Flavour =
+    | Vanilla
+    | Strawberry
+
+  type Event =
+    | IcecreamSold of Flavour
 
 
-type Event =
-  | Event1
-  | Event2
-  | Event3
+  // endregion
 
-// einfache Projektion
-type Projection<'State,'Event> =
-  {
-    InitialState : 'State
-    UpdateState : 'State -> 'Event list -> 'State
-  }
+  type Projector<'State,'Event> = 'State -> 'Event -> 'State
 
-
-type EventStore<'Event> = {
-  Append : 'Event list -> unit
-  Get :  unit -> 'Event list
-}
+  // einfache Projektion
+  type Projection<'State,'Event> =
+    {
+      InitialState : 'State
+      UpdateState : Projector<'State,'Event>
+    }
 
 
-  // 2 Beispiele in der Domäne
+  type EventStore<'Event> =
+    {
+      Append : 'Event list -> unit
+      Get :  unit -> 'Event list
+    }
 
-  // wie werden aus Events State
 
-  // einfach Funktionen, kein Subscriber. Wir müssen also die Events im State halten und abfragen
+    // 2 Beispiele in der Domäne
 
-  // Aktion holt sich auch den Stream und lässt die Projektion durchlaufen. Aber welchen Stream?
+    // wie werden aus Events State
 
-// brauchen möglichkeit State zu speichern, vllt ein Objekt?
-// Aktor erklären
-// Mailbox Prozessor erklären
-// brauchen eine Antwort
+    // einfach Funktionen, kein Subscriber. Wir müssen also die Events im State halten und abfragen
 
-// zeige Type Driven: erst später den Event Store implementieren
+    // Aktion holt sich auch den Stream und lässt die Projektion durchlaufen. Aber welchen Stream?
 
-// später Get für Stream
-// später: warte aufs Commit
+  // brauchen möglichkeit State zu speichern, vllt ein Objekt?
+  // Aktor erklären
+  // Mailbox Prozessor erklären
+  // brauchen eine Antwort
+
+  // zeige Type Driven: erst später den Event Store implementieren
+
+  // später Get für Stream
+  // später: warte aufs Commit
   type Msg<'Event> =
-  | Get of AsyncReplyChannel<'Event list>
-  | Append of 'Event list
+    | Get of AsyncReplyChannel<'Event list>
+    | Append of 'Event list
 
-let eventStore () : EventStore<'Event> =
-  let events = []
+  let eventStore () : EventStore<'Event> =
+    let events = []
 
-  let mailbox =
-    MailboxProcessor.Start(fun inbox ->
-      // printfn "Start"
-      let rec loop events =
-          async {
-            let! msg = inbox.Receive()
+    let mailbox =
+      MailboxProcessor.Start(fun inbox ->
+        // printfn "Start"
+        let rec loop events =
+            async {
+              let! msg = inbox.Receive()
 
-            match msg with
-            | Append newEvents ->
-                // printfn "EventStore received new events: %A" eventSet
-                return! loop (List.concat [ events ; newEvents ])
+              match msg with
+              | Append newEvents ->
+                  return! loop (List.concat [ events ; newEvents ])
 
-            | Get reply ->
-                reply.Reply events
+              | Get reply ->
+                  reply.Reply events
+                  return! loop events
+            }
 
-                return! loop events
-          }
+        loop events
+      )
 
-      loop events
-    )
+    let append events =
+      mailbox.Post (Append events)
 
-  let read() =
-    async {
-      return! mailbox.PostAndAsyncReply(Get)
-    } |> Async.RunSynchronously
+    {
+      Append = append
+      Get = fun () ->  mailbox.PostAndReply(Get)
+    }
 
 
-  let append events =
-    events |> Append |> mailbox.Post
+  let update state event =
+    match event with
+    | IcecreamSold flavour ->
+        flavour :: state
 
-  {
-    Append = append
-    Get = read
-  }
+  let soldIcecreamsProjection : Projection<Flavour list, Event> =
+    {
+      InitialState = []
+      UpdateState = update
+    }
+
+  // brauche irgendwie mehr events und Projektionen
+
+  let soldIceCreams getEvents =
+    let events =
+      getEvents()
+
+    events
+    |> List.fold soldIcecreamsProjection.UpdateState soldIcecreamsProjection.InitialState
+
+
+  // hier dann Program
+  let eventStoreReal : EventStore<Event> = eventStore()
+
+  eventStoreReal.Append [IcecreamSold Vanilla]
+  eventStoreReal.Append [IcecreamSold Strawberry]
+  eventStoreReal.Append [IcecreamSold Vanilla]
+  eventStoreReal.Append [IcecreamSold Vanilla]
+
+
+  let sold = soldIceCreams eventStoreReal.Get
+
+
+  // ab hier dann Aktionen
+  let sellIceCream flavour =
+    IcecreamSold flavour
+
+
+  // Erweiterung der Domäne
+
+  // wie viel in Stock
+  // Available, nicht available
+
+
+
+
 
 
 
