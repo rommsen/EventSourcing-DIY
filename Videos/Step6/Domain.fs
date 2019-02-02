56483e46867653514be8689d08e955881fa5c1ff
Roman Sachse <roman.sachse@googlemail.com>
2019-02-02 17:16:25 +0100

    Refactored to single files

---
+++ Videos/Step6/Domain.fs
@@ -0,0 +1,186 @@
+namespace Domain 
+
+type Flavour =
+| Vanilla
+| Strawberry
+
+type Event =
+| Flavour_sold of Flavour
+| Flavour_restocked of Flavour * int
+| Flavour_went_out_of_stock of Flavour
+| Flavour_was_not_in_stock of Flavour
+
+type Truck = Truck of System.Guid  
+
+
+module Projections =
+
+  open Infrastructure
+
+  let project projection events =
+    events |> List.fold projection.Update projection.Init
+
+  let soldOfFlavour flavour state =
+    state
+    |> Map.tryFind flavour
+    |> Option.defaultValue 0  
+
+  let private updateSoldFlavours state event =
+    match event with
+    | Flavour_sold flavour ->
+        state
+        |> soldOfFlavour flavour
+        |> fun portions -> state |> Map.add flavour (portions + 1)
+
+    | _ ->
+        state
+
+  let soldFlavours : Projection<Map<Flavour,int>, Event> =
+    {
+      Init = Map.empty
+      Update = updateSoldFlavours
+    }
+
+  let restock flavour number stock =
+    stock
+    |> Map.tryFind flavour
+    |> Option.defaultValue 0
+    |> fun portions -> stock |> Map.add flavour (portions + number)
+
+  let updateFlavoursInStock stock event =
+    match event with
+    | Flavour_sold flavour ->
+        stock |> restock flavour -1
+
+    | Flavour_restocked (flavour, portions) ->
+        stock |> restock flavour portions
+
+    | _ ->
+        stock
+
+  let flavoursInStock : Projection<Map<Flavour, int>, Event> =
+    {
+      Init = Map.empty
+      Update = updateFlavoursInStock
+    }
+
+  let stockOf flavour stock =
+    stock
+    |> Map.tryFind flavour
+    |> Option.defaultValue 0
+
+
+module Readmodels =
+  open Infrastructure 
+
+  type Query =
+    | Trucks 
+    | FlavoursInStock of Truck * Flavour
+
+  type ReadModel<'Event, 'Query, 'Result> =
+    {
+      EventListener : EventListener<'Event>
+      QueryHandler : QueryHandler<'Query,'Result>
+    } 
+
+    (*
+
+      Was erwarten wir von einem Readmodel
+      - spezifische Abfragen -> Query?
+      - projections für einen Stream
+      - projections für alle Streams
+
+
+      Ablauf:
+
+        - Wird gestartet
+        - Holt sich Events aus Event Store
+        - baut projection auf
+
+        - Events kommen an
+        - Readmodel entscheidet spezifisch wie es den state (für stream oder alle) holt (sql query, aus memory etc)
+        - Readmodel schickt state an projection
+        - projection generiert neuen State
+        - Readmodel hinterlegt state
+
+        - Query kommt an
+        - Readmodel prüft, ob es damit umgehen kann
+        - Readmodel entscheidet wie es damit umgeht
+        - gibt antwort zurück
+
+        Vorteil Memory: nichts anderes zu deployen etc
+        Nachteil: Serverstart, Speicherverbrauch
+
+        Zeigen: Behaviour kommt später, wir haben keinen Zugriff drauf
+
+        Man kann mehr logik in ein Readmodel bauen oder unterschiedliche
+    *)
+
+  type Msg<'Event,'Query,'Result> =
+    | Notify of StreamEvent<'Event>
+    | Query of 'Query * AsyncReplyChannel<QueryResult<'Result>>
+
+  let flavoursInStock () =
+    let agent =
+      let initState : Map<Stream, Map<Flavour, int>> = Map.empty  // hier record draus machen
+
+      MailboxProcessor.Start(fun inbox -> 
+        let rec loop state =
+          async {
+            let! msg = inbox.Receive() 
+
+            match msg with  
+            | Notify event ->
+                let newState =
+                  state
+                  |> Map.tryFind event.Stream
+                  |> Option.defaultValue Projections.flavoursInStock.Init
+                  |> fun projectionState -> event.Event |> Projections.flavoursInStock.Update projectionState
+                  |> fun newState -> state |> Map.add event.Stream newState
+
+                return! loop newState
+
+            | Query (query, reply) ->
+                let result =
+                  match query with  
+                  | FlavoursInStock (Truck truck, flavour) ->
+                      state 
+                      |> Map.tryFind truck
+                      |> Option.defaultValue Map.empty 
+                      |> Map.tryFind flavour 
+                      |> Option.defaultValue 0 
+                      |> Handled 
+
+                   | _ -> 
+                      NotHandled
+
+                result |> reply.Reply                                  
+          }
+
+        loop initState        
+      )
+
+    {
+      EventListener = Notify >> agent.Post
+      QueryHandler = fun query -> agent.PostAndReply(fun reply -> Query (query,reply))
+    }    
+
+
+module Behaviour =
+
+  open Projections
+
+  let sellFlavour flavour events =
+    let stock =
+      events
+      |> project flavoursInStock
+      |> stockOf flavour
+
+    match stock with
+    | 0 -> [Flavour_was_not_in_stock flavour]
+    | 1 -> [Flavour_sold flavour ; Flavour_went_out_of_stock flavour]
+    | _ -> [Flavour_sold flavour]
+
+
+  let restock flavour portions events =
+    [ Flavour_restocked (flavour,portions) ]
---
+++ Videos/Step6/Infrastructure.fs
@@ -0,0 +1,207 @@
+namespace Infrastructure
+
+type Stream = System.Guid
+
+type EventProducer<'Event> =
+  'Event list -> 'Event list
+
+
+type StreamEvent<'Event> =
+  {
+    Stream : Stream 
+    Event : 'Event
+  }  
+
+type EventListener<'Event> = 
+  StreamEvent<'Event> -> unit
+
+
+type EventStore<'Event> =
+  {
+    Get : unit -> StreamEvent<'Event> list
+    GetStream : Stream -> 'Event list
+    Append : Stream -> 'Event list -> unit
+    Evolve : Stream -> EventProducer<'Event> -> unit
+    Subscribe : EventListener<'Event>-> unit
+  }
+
+type Projection<'State,'Event> =
+  {
+    Init : 'State
+    Update : 'State -> 'Event -> 'State
+  }
+
+type QueryResult<'Result> =
+  | Handled of 'Result 
+  | NotHandled  
+
+type QueryHandler<'Query,'Result> =
+  'Query -> QueryResult<'Result>
+
+
+module EventStore =
+  type Msg<'Event> =
+    | Get of AsyncReplyChannel<StreamEvent<'Event> list>
+    | GetStream of Stream * AsyncReplyChannel<'Event list>
+    | Append of  Stream * 'Event list
+    | Evolve of Stream * EventProducer<'Event>
+    | Subscribe of EventListener<'Event>
+
+  let streamFor stream history =
+    history
+    |> List.filter (fun streamEvent -> streamEvent.Stream = stream)
+    
+  let asEvents streamEvents =
+    streamEvents 
+    |> List.map (fun streamEvent -> streamEvent.Event)
+
+  let asStreamEvents stream events =
+    events
+    |> List.map (fun event -> { Stream = stream ; Event = event })  
+
+  let appendFor stream history new_events stream_history =
+    new_events 
+    |> (@) stream_history
+
+  let notifyEventListeners events subscriptions =
+    subscriptions 
+    |> List.iter (fun subscription -> events |> List.iter subscription)
+
+  let initialize () : EventStore<'Event> =
+    let history : StreamEvent<'Event> list = []
+
+    let mailbox =
+      MailboxProcessor.Start(fun inbox ->
+        let rec loop (history,eventListeners : EventListener<'Event> list) =
+          async {
+            let! msg = inbox.Receive()
+
+            match msg with
+            | Get reply ->
+                reply.Reply history
+                return! loop (history,eventListeners)
+
+            | GetStream (stream,reply) ->
+                history
+                |> streamFor stream
+                |> asEvents
+                |> reply.Reply
+
+                return! loop (history,eventListeners)
+
+            | Append (stream,events)  ->
+                let new_history =
+                  history
+                  |> streamFor stream
+                  |> appendFor stream history (events |> asStreamEvents stream)
+
+                return! loop (new_history, eventListeners)
+
+            | Evolve (stream,producer) ->
+                let stream_history =
+                  history |> streamFor stream
+
+                let new_events =
+                  stream_history 
+                  |> asEvents
+                  |> producer
+                  |> asStreamEvents stream
+
+                let newHistory =
+                  appendFor stream history new_events stream_history 
+
+                do eventListeners |> notifyEventListeners new_events           
+
+                return! loop (newHistory, eventListeners)
+
+            | Subscribe listener ->
+                do history |> List.iter listener     
+
+                return! loop (history, listener :: eventListeners)
+
+                // Idee: gib möglichkeiten subscribern zum slicen mit
+
+                // Frage wohin wir optimieren wollen?
+                // Zugriffe auf Streams?
+                // alle Events
+                // Memory?
+                // es h#ngt davon ab wohin man optimieren möchte
+
+                // wenn alle Events dann möchte man sie schon in Order haben
+
+                // Eine Event Subscription braucht sie auf jeden Fall in der Order
+
+                // achtung Prototype: eventuell structs
+
+          }
+
+        loop (history,[])
+      )
+
+    let getStream aggregate =
+      mailbox.PostAndReply (fun reply -> (aggregate,reply) |> GetStream)
+
+    let append aggregate events =
+      (aggregate,events)
+      |> Append
+      |> mailbox.Post
+
+    let evolve aggregate producer =
+      (aggregate,producer)
+      |> Evolve
+      |> mailbox.Post
+
+    let subscribe (subscription : EventListener<_>) =
+      subscription
+      |> Subscribe
+      |> mailbox.Post    
+
+    {
+      Get = fun () ->  mailbox.PostAndReply Get
+      GetStream = getStream
+      Append = append
+      Evolve = evolve
+      Subscribe = subscribe
+    }
+
+
+module Queries =
+
+  type Msg<'Query,'Result> =
+    | Query of 'Query * AsyncReplyChannel<QueryResult<'Result>>
+
+  let rec private oneOf (queryHandler : QueryHandler<_,_> list) query =
+    match queryHandler with
+    | handler :: rest ->
+        match handler query with
+        | NotHandled ->
+            oneOf rest query
+
+        | Handled response ->
+            Handled response
+
+    | _ -> NotHandled
+
+  let queryHandler (queryHandler : QueryHandler<_,_> list) : QueryHandler<_,_> =
+    let agent =
+      MailboxProcessor.Start(fun inbox ->
+        let rec loop() =
+          async {
+            let! msg = inbox.Receive()
+
+            match msg with
+            | Query (query,reply)->
+                oneOf queryHandler query
+                |> reply.Reply
+
+            return! loop()
+          }
+
+        loop()
+      )
+
+    let queryHandler query =
+      agent.PostAndReply(fun reply -> Query (query,reply))
+
+    queryHandler
+  
