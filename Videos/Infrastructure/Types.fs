cbb8180e3a10309b9c9ddea9c383877ed340b4f0
Roman Sachse <roman.sachse@googlemail.com>
2019-07-01 11:45:54 +0200

    New Types for EventStore/Storage

--- Videos/Infrastructure/Types.fs
+++ Videos/Infrastructure/Types.fs
@@ -5,13 +5,37 @@ type EventSource = System.Guid
 type EventProducer<'Event> =
   'Event list -> 'Event list
 
+
+type EventMetadata =
+  {
+    Source : EventSource
+    RecordedAtUtc : System.DateTime
+  }
+
+type EventEnvelope<'Event> =
+  {
+    Metadata : EventMetadata
+    Event : 'Event
+  }
+
+type EventResult<'Event> =
+  Result<EventEnvelope<'Event> list, string>
+
 type EventStore<'Event> =
   {
-    Get : unit -> Map<EventSource,'Event list>
-    GetStream : EventSource -> 'Event list
-    Append : EventSource -> 'Event list -> unit
-    Evolve : EventSource -> EventProducer<'Event> -> unit
+    Get : unit -> Async<EventResult<'Event>>
+    GetStream : EventSource -> Async<EventResult<'Event>>
+    Append : EventEnvelope<'Event> list -> Async<Result<unit, string>>
+    Evolve : EventSource -> EventProducer<'Event> -> Async<Result<unit, string>>
   }
+
+type EventStorage<'Event> =
+  {
+    Get : unit -> Async<EventResult<'Event>>
+    GetStream : EventSource -> Async<EventResult<'Event>>
+    Append : EventEnvelope<'Event> list -> Async<unit>
+  }
+
 
 type Projection<'State,'Event> =
   {
